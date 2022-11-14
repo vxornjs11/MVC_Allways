9d677cb7e73009507304350b5b6d384181554a6d
javanyam <elyje5907@gmail.com>
2022-11-14 21:21:45 +0900

    mypoint

---
+++ Allways/src/main/java/com/jsplec/customer/dao/CCustomerEmailDao.java
@@ -0,0 +1,21 @@
+package com.jsplec.customer.dao;
+
+import java.util.Properties;
+import java.util.Random;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class CCustomerEmailDao {
+
+	
+	
+	
+	
+	
+} // End
